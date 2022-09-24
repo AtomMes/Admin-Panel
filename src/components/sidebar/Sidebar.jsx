03dbf66e9f93367fe1c2fb4aac98dfda0a14a4bd
AtomMes <atomik.2005@mail.ru>
2022-09-24 13:57:09 +0400

    SideBar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">ATOM</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">Main</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">Lists</p>
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">Useful</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Status</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">Service</p>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">User</p>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Sidebar />
+      <div className='homeContainer'>container</div>
+    </div>
+  );
+};
+
+export default Home;
